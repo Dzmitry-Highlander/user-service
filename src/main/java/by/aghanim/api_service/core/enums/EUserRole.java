d3cc0745fa39fbee77a5919286658c3254ac9969
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-17 20:30:28 +0300

    Add: EUserRole

---
+++ src/main/java/by/aghanim/api_service/core/enums/EUserRole.java
@@ -0,0 +1,14 @@
+package by.aghanim.api_service.core.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum EUserRole {
+    ROLE_ADMIN("Administrator"),
+    ROLE_USER("User"),
+    ROLE_SYSTEM("System");
+
+    private final String role;
+}
