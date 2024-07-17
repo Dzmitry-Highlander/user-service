7a74d91d59bbbf3d285d357b16d74d24e3c6060f
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-17 20:32:16 +0300

    Add: entity User

---
+++ src/main/java/by/aghanim/api_service/dao/entity/User.java
@@ -0,0 +1,68 @@
+package by.aghanim.api_service.dao.entity;
+
+import by.aghanim.api_service.core.enums.EUserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users", schema = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_uuid")
+    private UUID uuid;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private EUserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return mobileNumber;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
