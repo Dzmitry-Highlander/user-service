33408ad716129170f69c03f43a212d2885cb3e9f
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-17 19:47:27 +0300

    Add UserDTO

---
+++ src/main/java/by/aghanim/api_service/core/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package by.aghanim.api_service.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private UUID id;
+    private String nickname;
+    private String phoneNumber;
+}
