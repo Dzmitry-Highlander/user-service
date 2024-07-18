74324512c0e5a9cfc9281f7c8609928561bcb678
Dzmitriy Krasiuk <strixmymail@gmail.com>
2024-07-18 09:05:13 +0300

    Feat: @EnableConfigurationProperties

--- src/main/java/by/aghanim/user_service/UserServiceApplication.java
+++ src/main/java/by/aghanim/user_service/UserServiceApplication.java
@@ -1,12 +1,15 @@
 package by.aghanim.user_service;
 
 
+import by.aghanim.user_service.config.properties.JWTProperty;
+import by.aghanim.user_service.config.properties.MobileNumberProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//TODO @EnableConfigurationProperties({MailProperty.class, JWTProperty.class})
+@EnableConfigurationProperties({MobileNumberProperty.class, JWTProperty.class})
 @EnableJpaRepositories(basePackages = "by.aghanim.user_service.dao.api")
 @EnableTransactionManagement
 //TODO @EnableFeignClients
